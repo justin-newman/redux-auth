a507ba7f407fac10384dcab4e1cef39c20c63fde
Justin Newman <jnewman@cmnhospitals.org>
2017-05-31 23:09:58 -0600

    Completed Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Auth from './components/Auth';
+import Dashboard from './components/Dashboard';
+import FetchUser from './components/FetchUser';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => (
-  <p>
-    To get started, edit <code>src/App.js</code> and save to reload.
-  </p>
+  <div>
+    <NavBar />
+    <FetchUser> 
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route 
+          path="/register" 
+          render={ (props) => <Auth {...props} title="Register"/> } 
+        />
+        <Route
+          path="/login"
+          render={ (props) => <Auth {...props} title="Login" />}
+        />
+        <ProtectedRoute path="/dashboard" component={Dashboard} />
+      </Switch>
+    </FetchUser>
+  </div>
 );
 
 export default App;
---
+++ client/src/actions/user.js
@@ -0,0 +1,42 @@
+export const logout = () => {
+    return (dispatch) => {
+        fetch('/api/auth/sign_out', {
+            method: 'DELETE',
+            credentials: 'include'
+        }).then( () => dispatch(currentUser()) )
+    }
+}
+
+export const authenticate = (email, password, title, history) => {
+    return(dispatch) => {
+        let endpoint = title === 'Register' ? 'signup' : 'signin'
+        fetch(`/api/auth/${endpoint}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            credentials: 'include',
+            method: 'POST',
+            body: JSON.stringify({ email, password })
+        }).then( res => res.json() )
+          .then( user => {
+              dispatch(currentUser(user))
+              history.push('/dashboard')
+          })
+    }
+}
+
+export const tryFetchUser = (cb) => {
+    return (dispatch) => {
+        fetch('/api/auth/user', {
+            method: 'GET',
+            credentials: 'include'
+        }).then(res => res.json())
+            .then(user => dispatch(currentUser(user)))
+            .then(() => cb())
+    }
+} 
+
+const currentUser = (user = {}) => {
+    return { type: 'USER', user } 
+} 
